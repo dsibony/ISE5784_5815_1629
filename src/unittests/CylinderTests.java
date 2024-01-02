beeb0f8794d86f65ccf956cf1f8ad9fe1d354d8e
ASUS <ASUS@LAPTOP-QHT8DRET>
2024-01-02 17:58:44 +0200

    adding all the unittests

---
+++ src/unittests/CylinderTests.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for geometries.Cylinder class
+ */
+class CylinderTests {
+
+	/**
+	 * Test method for {@link geometries.Cylinder#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
