bb4f9e22569a1942ea8901810aaf915d15b79c5b
ASUS <ASUS@LAPTOP-QHT8DRET>
2024-02-04 18:54:57 +0200

    ImageWriterTest - Sibony

--- src/unittests/renderer/ImageWriterTest.java
+++ src/unittests/renderer/ImageWriterTest.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import primitives.Color;
+import renderer.ImageWriter;
+
 /**
  * 
  */
@@ -14,7 +17,20 @@ class ImageWriterTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		// ============ Equivalence Partitions Tests ==============
+		// TC01: regular image
+		ImageWriter image = new ImageWriter("test", 800, 500);
+		Color yellow = new Color(255, 255, 0);
+		Color red = new Color(255, 0, 0);
+		for (int i = 0; i < 800; i++) {
+			for (int j = 0; j < 500; j++) {
+				if (i % 50 == 0 || j % 50 == 0)
+					image.writePixel(i, j, red);
+				else
+					image.writePixel(i, j, yellow);
+			}
+		}
+		image.writeToImage();
 	}
 
 }
