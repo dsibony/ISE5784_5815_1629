db4f2860c81b1fb85b50905415db0923c537b4a6
ASUS <ASUS@LAPTOP-QHT8DRET>
2024-02-04 13:02:26 +0200

    Final new files - Sibony

---
+++ src/unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package unittests.renderer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+class ImageWriterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
