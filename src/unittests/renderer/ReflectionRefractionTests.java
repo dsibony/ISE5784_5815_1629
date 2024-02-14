d4810eaed0abc88c24865b5926eca6150898c467
ASUS <ASUS@LAPTOP-QHT8DRET>
2024-02-14 16:04:12 +0200

    Bonus for stage 7 - Sibony

--- src/unittests/renderer/ReflectionRefractionTests.java
+++ src/unittests/renderer/ReflectionRefractionTests.java
@@ -7,10 +7,8 @@ import static java.awt.Color.*;
 
 import org.junit.jupiter.api.Test;
 
-import geometries.Sphere;
-import geometries.Triangle;
-import lighting.AmbientLight;
-import lighting.SpotLight;
+import geometries.*;
+import lighting.*;
 import primitives.*;
 import renderer.*;
 import scene.Scene;
@@ -86,34 +84,73 @@ public class ReflectionRefractionTests {
 				.setImageWriter(new ImageWriter("refractionShadow", 600, 600)).build().renderImage().writeToImage();
 	}
 
+//	/**
+//	 * Produce a complicated picture of spheres lighted by a spot light
+//	 */
+//	@Test
+//	public void threeObjectsPicture() {
+//		scene.geometries.add(
+//				new Sphere(new Point(0, 0, -50), 50d).setEmission(new Color(GRAY))
+//						.setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setKt(0.3)),
+//				new Sphere(new Point(0, -12, -50), 10d).setEmission(new Color(RED))
+//						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)),
+//				new Sphere(new Point(10, 15, 10), 15d).setEmission(new Color(YELLOW))
+//						.setMaterial(new Material().setKd(0.1).setKs(0.3).setShininess(50).setKt(0.6)),
+//				new Sphere(new Point(-10, 15, 10), 15d).setEmission(new Color(YELLOW))
+//						.setMaterial(new Material().setKd(0.1).setKs(0.3).setShininess(50).setKt(0.6)),
+//				new Triangle(new Point(-30, -37, -40), new Point(30, -37, -50), new Point(0, -25, -45))
+//						.setEmission(new Color(ORANGE))
+//						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.3)),
+//				new Triangle(new Point(22, 28, 10), new Point(4, 32, 10), new Point(12, 37, 10))
+//						.setEmission(new Color(165, 42, 42))
+//						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.3)),
+//				new Triangle(new Point(-22, 28, 10), new Point(-4, 32, 10), new Point(-12, 37, 10))
+//						.setEmission(new Color(165, 42, 42))
+//						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.3)));
+//		scene.lights.add(new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2))
+//				.setKl(0.001).setKq(0.000006));
+//
+//		cameraBuilder.setP0(new Point(0, 0, 1000)).setVpDistance(1000).setVpSize(150, 150)
+//				.setImageWriter(new ImageWriter("faceObjectsPicture", 500, 500)).build().renderImage()
+//				.writeToImage();
+//	}
 	/**
-	 * Produce a complicated picture of spheres lighted by a spot light
+	 * Produce a complicated picture of spheres lighted by a spot light (Bonus)
 	 */
 	@Test
 	public void threeObjectsPicture() {
 		scene.geometries.add(
 				new Sphere(new Point(0, 0, -50), 50d).setEmission(new Color(GRAY))
-						.setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setKt(0.3)),
-				new Sphere(new Point(0, -12, -50), 10d).setEmission(new Color(RED))
+						.setMaterial(new Material().setKd(0.4).setKs(0.3).setShininess(100).setKt(0.35)),
+				new Sphere(new Point(0, -12, -30), 10d).setEmission(new Color(RED))
 						.setMaterial(new Material().setKd(0.5).setKs(0.5).setShininess(100)),
-				new Sphere(new Point(10, 15, 10), 15d).setEmission(new Color(YELLOW))
+				new Sphere(new Point(10, 15, -30), 15d).setEmission(new Color(YELLOW))
+						.setMaterial(new Material().setKd(0.1).setKs(0.3).setShininess(50).setKt(0.4)),
+				new Sphere(new Point(-10, 15, -30), 15d).setEmission(new Color(YELLOW))
+						.setMaterial(new Material().setKd(0.1).setKs(0.3).setShininess(50).setKt(0.4)),
+				new Sphere(new Point(100, 15, 50), 5d).setEmission(new Color(30, 30, 30))
 						.setMaterial(new Material().setKd(0.1).setKs(0.3).setShininess(50).setKt(0.6)),
-				new Sphere(new Point(-10, 15, 10), 15d).setEmission(new Color(YELLOW))
+				new Sphere(new Point(120, 15, 50), 5d).setEmission(new Color(30, 30, 30))
 						.setMaterial(new Material().setKd(0.1).setKs(0.3).setShininess(50).setKt(0.6)),
-				new Triangle(new Point(-30, -37, -40), new Point(30, -37, -50), new Point(0, -25, -45))
+				new Triangle(new Point(-30, -37, -30), new Point(30, -37, -40), new Point(0, -25, -35))
 						.setEmission(new Color(ORANGE))
-						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.3)),
-				new Triangle(new Point(22, 28, 10), new Point(4, 32, 10), new Point(12, 37, 10))
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.4)),
+				new Triangle(new Point(22, 28, -30), new Point(4, 32, -30), new Point(12, 37, -30))
 						.setEmission(new Color(165, 42, 42))
-						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.3)),
-				new Triangle(new Point(-22, 28, 10), new Point(-4, 32, 10), new Point(-12, 37, 10))
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.4)),
+				new Triangle(new Point(-22, 28, -30), new Point(-4, 32, -30), new Point(-12, 37, -30))
 						.setEmission(new Color(165, 42, 42))
-						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.3)));
-		scene.lights.add(new SpotLight(new Color(1000, 600, 0), new Point(-100, -100, 500), new Vector(-1, -1, -2))
+						.setMaterial(new Material().setKd(0.2).setKs(0.2).setShininess(50).setKt(0.4)),
+				new Sphere(new Point(-200, 0, -30), 50d).setEmission(new Color(20, 20, 20))
+						.setMaterial(new Material().setKr(1)),
+				new Plane(new Point(-200, 0, -30), new Vector(-1.5, 0, -1)).setEmission(new Color(20, 20, 20))
+						.setMaterial(new Material().setKr(1)));
+		scene.lights.add(new SpotLight(new Color(1000, 600, 0), new Point(-200, -100, 500), new Vector(-1, -1, -2))
 				.setKl(0.001).setKq(0.000006));
 
-		cameraBuilder.setP0(new Point(0, 0, 1000)).setVpDistance(1000).setVpSize(150, 150)
-				.setImageWriter(new ImageWriter("complicateObjectsPicture", 500, 500)).build().renderImage()
+		cameraBuilder.setP0(new Point(1002, 0, 668)).setVpDistance(800).setVpSize(150, 150)
+				.setDirection(new Vector(-1.5, 0, -1), new Vector(0, 1, 0))
+				.setImageWriter(new ImageWriter("complicateObjectsPicture", 1000, 1000)).build().renderImage()
 				.writeToImage();
 	}
 }
